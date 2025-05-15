d07355a4120a519c3c30fd69f9250e3d0e9afae3
Gabriel Tiso <gabriel.tiso@proton.me>
2025-05-15 19:47:48 -0300

    fix: remove wrong case files

--- src/infra/ws/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Infra\Ws;
-
-require __DIR__ . '/../../../vendor/autoload.php';
-
-use App\Models\Player;
-
-echo "Hello from the WebSocket server!\n";
-echo Player::createSimple("test")->name() . "\n";
